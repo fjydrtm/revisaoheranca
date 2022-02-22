4693b878956fc4dc5552615ba19b4a4ae6aec3ef
fjydrtm <enzo_tsutsumi@hotmail.com>
2022-02-22 17:51:02 +0000

    inclusão da classe empregado

---
+++ Empregado.java
@@ -0,0 +1,13 @@
+public class Empregado extends Pessoa{
+
+    private double salario;
+
+    public double getSalario(){
+        return this.salario;
+    }
+
+    public void double setSalario(double salario){
+        this.salario=salario;
+    }
+    
+}
